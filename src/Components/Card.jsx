e9adb6d1359ed35784899a9678e70b7abbd14bb1
Andy Caseres <andycaseres2@gmail.com>
2022-03-23 16:17:17 -0500

    create Datecity and functionlity search

---
+++ src/Components/Card.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { datecity } from "./datecity";
+import { API_URL, API_KEY } from "../services/setting";
+
+const Card = () => {
+  const [query, setquery] = useState("");
+  const [weather, setweather] = useState({});
+
+  const search = (evt) => {
+    if (evt.key === "Enter") {
+      fetch(`${API_URL}weather?q=${query}&units=metric&APPID=${API_KEY}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setweather(result);
+          setquery("");
+          console.log(result);
+        });
+    }
+  };
+
+  return (
+    <div>
+      <h1 className="title">AcClima</h1>
+      <main>
+        <div>
+          <input
+            className="search"
+            type="text"
+            placeholder="Search..."
+            onChange={(e) => setquery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {typeof weather.main != "undefined" ? (
+          <div>
+            <div className="container-location">
+              <div className="location">
+                {weather.name} , {weather.sys.country}
+              </div>
+              <div className="data">{datecity(new Date())}</div>
+            </div>
+            <div className="container-weather">
+              <div className="temp">{Math.round(weather.main.temp)}ºC</div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </main>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/Components/datecity.js
@@ -0,0 +1,32 @@
+export const datecity = (d) => {
+  let months = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+  ];
+  let days = [
+    "Domingo",
+    "Lunes",
+    "Martes",
+    "Miercoles",
+    "Jueves",
+    "Viernes",
+    "Sabado",
+  ];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+};
